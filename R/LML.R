25eb1569726cc8f536b1d0e40262b827a3152797
Matthew Parker <mrparker909@gmail.com>
2019-09-12 11:07:08 -0700

    updated manual

--- R/LML.R
+++ R/LML.R
@@ -1,5 +1,5 @@
 #' @title LMList
-#' @description Limited Memory List. Adds new element to top of list, removing bottom elements if new length is greater tham M.
+#' @description Limited Memory List. Adds new element to top of list, removing bottom elements if new length is greater than M.
 #' @param new_el   element to add to top of list
 #' @param old_list list to append new_el to
 #' @param M        maximum size of list (will truncate by removing bottom if length is greater than M)
@@ -27,8 +27,8 @@ LMList <- function(new_el, old_list = list(), M = 1) {
 #' @title LMc
 #' @description Limited Memory concatenate. Adds new elements to front, removing oldest elements if new length is greater tham M
 #' @param new_el   elements to add to top of list, if length(new_el) > 1, then the new elements will be appended left to right (so that the last element will be the most recent).
-#' @param old_list elements to concatenate new_el with
-#' @param M        maximum number of elements (will truncate by removing oldest if length is greater than M)
+#' @param old_list elements to concatenate new_el with.
+#' @param M        Memory, maximum number of elements (will truncate by removing oldest if length is greater than M)
 #' @examples
 #' a = LMc(1, M=5)
 #' print(a)
